d292776a8ec1d5a7add055b03aea491a2d7a51fc
cesarluchesi <cesarcesar.luchesi@dbccompany.com>
2023-03-10 08:05:18 -0300

    feat: aula 21 modulo 3

---
+++ modulo-03/aula21.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+<= menor uqe ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito(valor e tipo)
+
+
+*/
