3c0e5099c88488916f02394e39579c920f9f1337
Cesar Luchesi <cesarfluchesi123@gmail.com>
2024-06-18 07:44:58 -0300

    JS udemy

---
+++ node/aula01/app.js
@@ -0,0 +1,5 @@
+const {Pessoa} = require('./mode1')
+const path = require('path')
+const axios = require('axios')
+
+const p1 = new Pessoa('Cesar')
---
+++ node/aula04/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs').promises
+const path = require('path')
+
+async function readdir(rootDir){
+    rootDir = rootDir || path.resolve(__dirname)
+    const files = await fs.readdir(rootDir)
+    walk(files)
+}
+
+function walk(files){
+    for(let file of files){
+        console.log(file)
+    }
+}
+
+readdir('/Cesar/Programação/UdemyJS/')
