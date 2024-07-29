df0aeabc28b790cec17cf0289dd09cd061e42541
Cesar Luchesi <cesarfluchesi123@gmail.com>
2024-07-28 21:24:08 -0300

    final session 10

--- node/aula16/server.js
+++ node/aula16/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const port = 3000
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.CONNECTIONSTRING)
@@ -10,16 +11,18 @@ mongoose.connect(process.env.CONNECTIONSTRING)
  })
  .catch(e => console.log(e))
 
- const session = require('express-session')
- const MongoStore = require('connect-mongo')
- const flash = require('connect-flash')
+  const session = require('express-session')
+  const MongoStore = require('connect-mongo')
+  const flash = require('connect-flash')
+  const routes = require('./routes')
+  const path = require('path')
+  const helmet = require('helmet')
+  const { middlewareGlobal, checkCsrfError,csrfMiddleware } = require('./src/middlewares/middleware')
+  const csrf = require('csurf')
 
-const port = 3000
-const routes = require('./routes')
-const path = require('path')
-const { middlewareGlobal } = require('./src/middlewares/middleware')
-
+app.use(helmet())
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 const sessionOptions = session({
@@ -38,7 +41,10 @@ app.use(flash())
 app.set('views',path.resolve( __dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
+app.use(csrf())
 app.use(middlewareGlobal)
+app.use(checkCsrfError)
+app.use(csrfMiddleware)
 app.use(routes)
 
 app.on('Conectado', () =>{
--- node/aula16/src/controllers/homeController.js
+++ node/aula16/src/controllers/homeController.js
@@ -1,5 +1,6 @@
 exports.homePage = (req, res,) => {
-    res.render('index')
+    res.render('index', {
+    })
     return
 }
 
